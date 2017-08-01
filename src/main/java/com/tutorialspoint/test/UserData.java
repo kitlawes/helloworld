17c1853ca6f109e9e4fff453a0cb8a2a718efc76
Kit Lawes <kitlawes@hotmail.com>
2017-08-01 16:51:46 +0100

    Implement an Ajax asynchronous request and response

--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -10,7 +10,6 @@ import javax.faces.bean.SessionScoped;
 public class UserData implements Serializable {
    private static final long serialVersionUID = 1L;
    private String name;
-   private String password;
    
    public String getName() {
       return name;
@@ -19,16 +18,8 @@ public class UserData implements Serializable {
    public void setName(String name) {
       this.name = name;
    }
-   
-   public String getPassword() {
-      return password;
+
+   public String getWelcomeMessage() {
+      return "Hello " + name;
    }
-   
-   public void setPassword(String password) {
-      this.password = password;
-   }	
-   
-   public String login() {
-      return "result";
-   }	
 }
