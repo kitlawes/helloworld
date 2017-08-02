27739c58e6f0a2ad15d38abdd68fe7388d0d488c
Kit Lawes <kitlawes@hotmail.com>
2017-08-02 11:14:29 +0100

    Fix issue with Java Database Connectivity integration

--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -17,47 +17,55 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "userData", eager = true)
 @SessionScoped
 public class UserData implements Serializable {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   public List<Author> getAuthors() {
-      ResultSet rs = null;
-      PreparedStatement pst = null;
-      Connection con = getConnection();
-      String stm = "Select * from authors";
-      List<Author> records = new ArrayList<Author>();
-      
-      try {   
-         pst = con.prepareStatement(stm);
-         pst.execute();
-         rs = pst.getResultSet();
+	public List<Author> getAuthors() {
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+		Connection con = getConnection();
+		String stm = "Select * from authors";
+		List<Author> records = new ArrayList<Author>();
 
-         while(rs.next()) {
-            Author author = new Author();
-            author.setId(rs.getInt(1));
-            author.setName(rs.getString(2));
-            records.add(author);				
-         }
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      return records;
-   }
+		try {
+			pst = con.prepareStatement(stm);
+			pst.execute();
+			rs = pst.getResultSet();
 
-   public Connection getConnection() {
-      Connection con = null;
-      String url = "jdbc:postgresql://localhost/testdb";
-      String user = "user1";
-      String password = "user1";
-      
-      try {
-         con = DriverManager.getConnection(url, user, password);
-         System.out.println("Connection completed.");
-      } catch (SQLException ex) {
-         System.out.println(ex.getMessage());
-      }
-      
-      finally {
-      }
-      return con;
-   }
+			while (rs.next()) {
+				Author author = new Author();
+				author.setId(rs.getInt(1));
+				author.setName(rs.getString(2));
+				records.add(author);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return records;
+	}
+
+	public Connection getConnection() {
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Connection con = null;
+		String url = "jdbc:postgresql://localhost/testdb";
+		String user = "user1";
+		String password = "user1";
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection completed.");
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+		finally {
+		}
+		System.out.println("Connection: " + con);
+		return con;
+	}
 }
